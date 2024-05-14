11911861d03a2b07477ccfcf6ac0ddfaf531ac87
fagoon1311 <fs14smailbox65@gmail.com>
2024-05-14 17:50:16 +0530

    rxp

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,25 +1,41 @@
 import React from 'react'
 import { EXPERIENCES } from '../constants'
-
+import { motion } from 'framer-motion'
 import {experience} from '../constants'
 const Experience = () => {
   return (
     <div className='border-b border-neutral-900 pb-4'>
-        <h1 className='my-20 text-center text-4xl'> Experience</h1>
+        <motion.h1 
+        whileInView={{opacity: 1, y: 0}}
+        initial={{opacity:0, y:-100}}
+        transition={{duration: 0.5}}
+        className='my-20 text-center text-4xl'>
+            Experience
+        </motion.h1>
         <div>
             {EXPERIENCES.map((exp, index)=>(
                 <div key={index}
                 className='mb-8 flex flex-wrap lg:justify-center'>
-                    <div className='w-full lg:w-1/4'>
+                    <motion.div 
+                    className='w-full lg:w-1/4'
+                    whileInView={{opacity: 1, x:0}}
+                    initial={{opacity:0, x:-100}}
+                    transition={{duration:0.5}}
+                    >
                         <p className='mb-2 text-sm text-neutral-400'>{exp.year}</p>
-                    </div>
-                    <div className='w-full max-w-xl lg:w-3/4'>
+                    </motion.div>
+                    <motion.div 
+                    className='w-full max-w-xl lg:w-3/4'
+                    whileInView={{opacity: 1, x:0}}
+                    initial={{opacity:0, x:100}}
+                    transition={{duration:0.5}}
+                    >
                         <h6 className='mb-2 font-semibold'>{exp.role}-<span className='text-sm text-purple-500'>{exp.company}</span></h6>
                         <p className='mb-4 text-neutral-400'>{exp.description}</p>
                         {exp.technologies.map((tech, index)=>(
                             <span className="px-2 text-sm font-medium text-purple-500 mr-2 bg-neutral-900 rounded-lg"key={index}>{tech}</span>
                         ))}
-                    </div>
+                    </motion.div>
 
                 </div>
             ))}
