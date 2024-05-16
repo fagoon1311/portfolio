935444ce2e0e775c0b16b023e1b7450f9d24adcf
fagoon1311 <fs14smailbox65@gmail.com>
2024-05-16 23:20:42 +0530

    d

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,7 +20,7 @@ const Hero = () => {
         <div className='border-b border-neutral-900 pb-4 lg:mb-35'>
             <div className='flex flex-wrap'>
                 <div className='w-full lg:w-1/2'>
-                    <div className='flex flex-col items-center lg:items-start'>
+                    <div className='flex flex-col items-center lg:items-start p-12'>
                         <motion.h1 
                             variants={container(0)}
                             initial="hidden"
